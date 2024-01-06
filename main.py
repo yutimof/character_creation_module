f3d17361a27eb2b520334a08b6930ee998088b98
yutimof <yutimof@gmail.com>
2024-01-06 23:45:11 +0100

    Add start_game_banner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from start_game_banner import run_screensaver
 
 
 def attack(char_name, char_class):
@@ -79,6 +80,7 @@ def choice_char_class():
 
 
 def main():
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
